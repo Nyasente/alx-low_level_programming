60f5b2dd98743840dc9507e1b5f8cf2c8d3e6d97
Nyasente <4984ramock@gmail.com>
2023-10-05 15:31:14 -0700

    hello print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - piece of art
+ * Return: 0 (wala!)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,%d\n");
+	return (0);
+}
