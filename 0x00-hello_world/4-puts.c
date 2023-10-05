2cab9d3be13b7b193c80d42d24876dba07059056
Nyasente <4984ramock@gmail.com>
2023-10-05 15:02:00 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h
+/**
+ *main - this is a program using puts
+ * Return: 0 (yay)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
