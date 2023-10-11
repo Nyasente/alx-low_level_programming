3728e5536ff3d68bb7ad4264d384a93999cd1267
Nyasente <4984ramock@gmail.com>
2023-10-11 15:31:55 -0700

    insomia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
-*/
-int main(void)
+ * print alphabets - i.e lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
