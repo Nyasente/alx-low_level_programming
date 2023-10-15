a40c96c395218239c5a6d4c6e2cfdaa4eb351001
Nyasente <4984ramock@gmail.com>
2023-10-15 12:06:41 -0700

    100 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,38 +2,35 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- * Return empty output
  */
-
 void times_table(void)
-
 {
-	int i, j, k, l, m;
-	for (i = 0; i <= 0; i++)
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j <= 0; j++)
-	{
-	k = i * j;
-	if (k > 0)
-	{
-	l = k % 10;
-	m = (k - l) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(m + '0');
-	_putchar(l + '0');
-	}
-	else
-	{
-	if (j != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(k + '0');
-	}
-	}
-	_putchar('\n');
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
