92fdf4949bbe15c2d57bdab386b4ca051f4fae88
Nyasente <4984ramock@gmail.com>
2023-10-29 11:15:38 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _islower - checks characters
+ * @c: function parameter
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
