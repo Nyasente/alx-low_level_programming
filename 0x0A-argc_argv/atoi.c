843f4c7819d299b084f64ec070feb42bb9cf2c3c
Nyasente <4984ramock@gmail.com>
2023-11-01 09:10:27 -0700

    function prototype atoi

---
+++ 0x0A-argc_argv/atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "stdlib.h"
+#include "main.h"
+#include "string.h"
+/**
+ * _atoi - main code
+ * @s: string to print
+ * return: 0
+ */
+int atoi(char *s);
+{
+return (atoi(s));
+}
