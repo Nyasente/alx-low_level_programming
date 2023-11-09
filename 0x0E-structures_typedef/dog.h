cb722d28242f34618ba9695563dbfb9ecb1f47bd
Nyasente <4984ramock@gmail.com>
2023-11-08 19:22:44 -0800

    copy dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure to be used
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: long description for dog
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
