569457ed28041c88e97589dd963db9c465037307
Nyasente <4984ramock@gmail.com>
2024-02-05 22:29:50 -0800

    linked well

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,25 +10,17 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *temp = *head;
-	unsigned int len = 0;
+	list_t *temp;
+	int length = 0;
 
-	while (str[len])
-		len++;
-	new = malloc(sizeof(list_t));
-	if (!new)
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = len;
-	new->next = NULL;
-	if (head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-	return (new);
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
